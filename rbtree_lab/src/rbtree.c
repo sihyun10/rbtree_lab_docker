74d377f62c71da4f3df795417f1fa3b78fa7ce42
sihyun10 <hyhy200170@gmail.com>
2025-04-21 20:11:40 +0900

    feat: new_rbtree(), left_rotate(), right_rotate() 구현 #2
    레드-블랙 트리의 삽입 함수를 구현하기 전에 필요한 기능을 구현하였다.
    RB트리 구조체를 생성하고 초기화하는 역할을 하는 new_rbtree()와
    RB트리에서 삽입 또는 삭제 후에 균형을 맞추기 위해 회전을 사용하는데,
    2가지(좌회전, 우회전) 함수가 필요하기에 구현해주었다.

--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -2,43 +2,124 @@
 
 #include <stdlib.h>
 
-rbtree *new_rbtree(void) {
+rbtree *new_rbtree(void)
+{
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
+  if (p == NULL)
+    return NULL;
+
+  p->nil = (node_t *)calloc(1, sizeof(node_t));
+  if (p->nil == NULL)
+  {
+    free(p);
+    return NULL;
+  }
+
+  p->nil->color = RBTREE_BLACK;
+  p->nil->left = p->nil->right = p->nil->parent = NULL;
+
+  p->root = p->nil;
+
   return p;
 }
 
-void delete_rbtree(rbtree *t) {
+// 좌회전, 우회전 구현
+void left_rotate(rbtree *t, node_t *x)
+{
+  node_t *y = x->right;
+  x->right = y->left;
+
+  if (y->left != t->nil)
+  {
+    y->left->parent = x;
+  }
+
+  y->parent = x->parent;
+
+  if (x->parent == t->nil)
+  {
+    t->root = y;
+  }
+  else if (x == x->parent->left)
+  {
+    x->parent->left = y;
+  }
+  else
+  {
+    x->parent->right = y;
+  }
+
+  y->left = x;
+  x->parent = y;
+}
+
+void right_rotate(rbtree *t, node_t *y)
+{
+  node_t *x = y->left;
+  y->left = x->right;
+
+  if (x->right != t->nil)
+  {
+    x->right->parent = y;
+  }
+
+  x->parent = y->parent;
+
+  if (y->parent == t->nil)
+  {
+    t->root = x;
+  }
+  else if (y == y->parent->right)
+  {
+    y->parent->right = x;
+  }
+  else
+  {
+    y->parent->left = x;
+  }
+
+  x->right = y;
+  y->parent = x;
+}
+
+void delete_rbtree(rbtree *t)
+{
   // TODO: reclaim the tree nodes's memory
   free(t);
 }
 
-node_t *rbtree_insert(rbtree *t, const key_t key) {
+node_t *rbtree_insert(rbtree *t, const key_t key)
+{
   // TODO: implement insert
   return t->root;
 }
 
-node_t *rbtree_find(const rbtree *t, const key_t key) {
+node_t *rbtree_find(const rbtree *t, const key_t key)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_min(const rbtree *t) {
+node_t *rbtree_min(const rbtree *t)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_max(const rbtree *t) {
+node_t *rbtree_max(const rbtree *t)
+{
   // TODO: implement find
   return t->root;
 }
 
-int rbtree_erase(rbtree *t, node_t *p) {
+int rbtree_erase(rbtree *t, node_t *p)
+{
   // TODO: implement erase
   return 0;
 }
 
-int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
+int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
+{
   // TODO: implement to_array
   return 0;
 }
